055fa6c9703d6b0ea647431e238cad05214260c9
Leonid Tsukanov <sv1nnet1992@gmail.com>
2019-07-11 22:33:54 +0300

    User schema, authentication with jwt

--- server/models/user.js
+++ server/models/user.js
@@ -1,20 +1,78 @@
+/* eslint-disable no-underscore-dangle */
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-const User = mongoose.model('User', {
+const UserSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
     minlength: 5,
     trim: true,
+    unique: true,
+    validate: {
+      validator(email) {
+        return validator.isEmail(email);
+      },
+      message: '{VALUE} is not a valid email.',
+    },
   },
-  name: {
+  password: {
     type: String,
     required: true,
-    minlength: 1,
-    trim: true,
+    minlength: 8,
   },
-
+  tokens: [
+    {
+      access: {
+        type: String,
+        required: true,
+      },
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
+
+UserSchema.methods.toJSON = function toJSON() {
+  const user = this;
+  const userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function generateAuthToken() {
+  const user = this;
+
+  const access = 'auth';
+  const token = jwt.sign({ _id: user._id.toHexString(), access }, 'secret').toString();
+
+  user.tokens = user.tokens.concat([{ access, token }]);
+
+  return user.save().then(() => token);
+};
+
+UserSchema.statics.findByToken = function findByToken(token) {
+  const User = this;
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, 'secret');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    _id: decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth',
+  });
+};
+
+const User = mongoose.model('User', UserSchema);
 
 module.exports = {
   User,
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const _ = require('lodash');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+const { authenticate } = require('./middleware/authenticate');
 
 const app = express();
 
@@ -91,6 +92,24 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((err) => {
     res.status(400).send();
   });
+});
+
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  const user = new User(body);
+
+  user.save()
+    .then(() => user.generateAuthToken())
+    .then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(3111, () => {
