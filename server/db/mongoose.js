d9bbf9ba738d1441bf3285363d5aa032c0282e8d
Leonid Tsukanov <sv1nnet1992@gmail.com>
2019-07-15 16:10:58 +0300

    Test env vars for tests added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
-mongoose.connect(`${process.env.MONGODB_IP}/Todo`);
+mongoose.connect(process.env.MONGODB_URI);
 
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -1,3 +1,14 @@
+const env = process.env.NODE_ENV || 'development';
+console.log('env *********', env);
+
+if (env === 'development') {
+  process.env.PORT = 3111;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/Todo';
+} else if (env === 'test') {
+  process.env.PORT = 3111;
+  process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoTest';
+}
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
@@ -9,13 +20,14 @@ const todosRouter = require('./routes/todos');
 const usersRouter = require('./routes/users');
 
 const app = express();
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
 app.use('/todos', todosRouter);
 app.use('/users', usersRouter);
 
-app.listen(3111, () => {
+app.listen(port, () => {
   console.log('Started on port 3111');
 });
 
